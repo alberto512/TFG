c171ce51997e96a8fafd442100fc176ecf64b498
Alberto <alberto512c@gmail.com>
2023-04-01 13:30:34 +0200

    fix: update santander payload

--- backend/internal/santander/santander.go
+++ backend/internal/santander/santander.go
@@ -11,7 +11,7 @@ func AuthorizeOpenBusiness(code string) (string, error) {
 
 	url := "https://apis-sandbox.bancosantander.es/canales-digitales/sb/v2/authorize/?client_id=bc75ee49-9924-4160-904e-6b246d751e2c&redirect_uri=https://tfg-app.netlify.app&response_type=code"
 
-	payload := strings.NewReader("{\"access\":{\"accounts\":[{\"currency\":\"EUR\",\"msisdn\":\"…\",\"bban\":\"20385778983000760236\",\"iban\":\"ES1111111111111111111\"}],\"balances\":[{\"currency\":\"EUR\",\"msisdn\":\"…\",\"bban\":\"20385778983000760236\",\"iban\":\"ES1111111111111111111\"}],\"transactions\":[{\"currency\":\"EUR\",\"msisdn\":\"…\",\"bban\":\"20385778983000760236\",\"iban\":\"ES1111111111111111111\"}],\"allPsd2\":\"allAccounts\",\"availableAccounts\":\"allAccounts\",\"availableAccountsWithBalances\":\"6376496895865296\",\"cards_accounts\":[{\"resourceId\":\"732692424631\"}],\"cards_balances\":[{\"resourceId\":\"732692424631\"}],\"cards_transactions\":[{\"resourceId\":\"732692424631\"}]},\"recurringIndicator\":\"true\",\"frequencyPerDay\":\"5\",\"validUntil\":\"2019-09-20\",\"scopes\":\"[\\\"myprofile.read\\\"]\"}")
+	payload := strings.NewReader("{\"access\":{\"accounts\":[],\"balances\":[],\"transactions\":[],\"cards_accounts\":[],\"cards_balances\":[],\"cards_transactions\":[]},\"recurringIndicator\":\"true\",\"frequencyPerDay\":\"5\",\"validUntil\":\"2023-09-20\"}")
 
 	req, _ := http.NewRequest("POST", url, payload)
 
