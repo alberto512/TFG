aebd72f3c20bcfbc1e88a5db69b76fd02dfb7c79
Alberto <alberto512c@gmail.com>
2023-04-12 22:36:08 +0200

    feature: show data

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useAuth } from 'components/authProvider/AuthProvider';
 import './Dashboard.css';
@@ -6,6 +6,7 @@ import './Dashboard.css';
 const Dashboard = () => {
   const { token } = useAuth();
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
+  const [accounts, setAccounts] = useState([]);
 
   const getAccounts = async () => {
     const response = await axios.post(
@@ -23,7 +24,19 @@ const Dashboard = () => {
 
     const responseDecoded = JSON.parse(response.data.data.accountsByToken);
 
-    console.log(responseDecoded);
+    const accounts = [];
+
+    for (const account in responseDecoded) {
+      accounts.push({
+        iban: responseDecoded[account][0].iban,
+        type: responseDecoded[account][0].name,
+        currency: responseDecoded[account][0].currency,
+        ammount: responseDecoded[account][0].balance.amount,
+        transactions: responseDecoded[account][1].account.transactions,
+      });
+    }
+
+    setAccounts(accounts);
   };
 
   useEffect(() => {
@@ -35,6 +48,26 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
 
       <div>Authenticated</div>
+      <ul>
+        {accounts.map((account) => (
+          <li>
+            <span>
+              {account.iban} | {account.type} | {account.currency} |{' '}
+              {account.ammount}
+            </span>
+            <ul>
+              {account.transactions.map((transaction) => (
+                <li>
+                  <span>
+                    {transaction.valueDate} | {transaction.description} |{' '}
+                    {transaction.amount.content}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
